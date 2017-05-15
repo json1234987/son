d4f57fb114935d7e37cf279795f2000bf9dfd02d
ESS Mac Pro <ESSMacPro@essmacbookpro.local>
2017-05-15 06:28:55 -0400

    Initial Commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by ESS Mac Pro on 5/15/17.
+//  Copyright © 2017 Pyar.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
